97b44c90ea37c969a3a65cfcbd80b87837a24aec
sahil <sahiillx02@gmail.com>
2024-12-14 12:07:12 +0530

    Initial commit

---
+++ demo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <nav>
+        <ul className='d-flex list-unstyled gap-5 justify-content-center'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>More</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
